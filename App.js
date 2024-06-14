f856e5efd47d8e70ca3a36fb78530a8deabe0f3e
makmur21 <136968703+makmur21@users.noreply.github.com>
2024-06-14 10:58:30 +0700

    boxsing

--- App.js
+++ App.js
@@ -3,35 +3,35 @@ import React from "react"
 
 const App = () => {
   return (
-    <View style={{
+    <View style = {{
       flex:1
     }}>
       <View style={{
-        flex:1,
-        flexDirection: 'row',
-      }}>
-        </View>
-      <View style={{
-        flex:1,
-        backgroundColor:"red"
-      }}>
-      </View>
-      <View style={{
-        flex:1,
-        backgroundColor:"yollow"
-      }}>
-      </View>
-      <View style={{
-        flex:1,
-        backgroundColor:"black"
-      }}>
-      <View style={{
-        flex:1,
-        backgroundColor:"blue"
-      }}>
+      justifyContent: "flex-start",//baris
+      alignItems:"flex-start",//kolom
+    }}>
+    <View style={{
+      width:100,
+      height:100,
+      backgroundColor:"red",
+    }}>
+    </View>
+    </View>
+    <View style = {{
+      flex:1,
+      justifyContent: "flex-end",//baris
+      alignItems:"flex-end",//kolom
+    }}>
+    <View style={{
+      width:100,
+      height:100,
+      backgroundColor:"blue",
+    }}>
     </View>
     </View>
     </View>
   )
-}
-export default App
+
+ }
+  export default App
+      
