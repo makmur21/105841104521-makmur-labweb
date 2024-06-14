074694f0572e98d72bacfd919bbd7517d8e00b92
makmur21 <136968703+makmur21@users.noreply.github.com>
2024-06-14 15:57:49 +0700

    tugas 2

--- App.js
+++ App.js
@@ -3,28 +3,73 @@ import React from "react"
 
 const App = () => {
   return (
-      <View style = {{
-        flex:1,
-      justifyContent: "center",//baris
-      alignItems:"center",//kolom
-    }}>
-    <View style={{
-      width:100,
-      height:100,
-      backgroundColor:"red",
-      alignSelf:"center",
-    }}>
+      <View 
+      style ={{
+        flex: 1,
+        //alignitems:kolom
+        //justifycontent:baris;
+        flexDirection:"row"
+      }}>
+      <View
+      style={{
+        flex: 1,
+        alignItems:"flex-end",
+        justifyContent:"center",
+      }}>
+        <View
+        style={{
+          width: 140,
+          height: 80,
+          backgroundColor: "red",
+          borderRadius: 10,
+          marginRight: 10,
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+          <Text
+          style={{
+            color:"white",
+            fontSize:25,
+          }}>
+            SIGN IN
+          </Text>
+        </View>
       </View>
-    <View style={{
-      width:100,
-      height:100,
-      backgroundColor:"blue",
-      alignSelf:"center",
-    }}>
-    </View>
-    </View>
-  )
 
+      <View
+      style={{
+        flex: 1,
+        alignItems:"flex-start",
+        justifyContent: "center",
+      }}>
+        <View
+        style={{
+          flex: 1,
+          alignItems:"flex-start",
+          justifyContent: "center",
+        }}>
+          <View
+          style={{
+            width:140,
+            height:80,
+            backgroundColor:"blue",
+            borderRadius:10,
+            marginLeft:10,
+            justifyContent:"center",
+            alignItems:"center",
+          }}>
+            <Text
+            style={{
+              color: "white",
+              fontSize: 25,
+            }}>
+              SIGN UP
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+      );
  }
   export default App
       
