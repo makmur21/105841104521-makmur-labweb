30cdc762ea4b079f9ac90b8c18e4d86571bf19f2
makmur21 <136968703+makmur21@users.noreply.github.com>
2024-07-19 22:35:34 +0700

    navigation dan taksabr

--- App.js
+++ App.js
@@ -3,9 +3,18 @@ import { View, Text, Button, Image, TouchableOpacity, TouchableOpacityBase } fro
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginPage from './src/LoginPage';
+import Bag from './Bag';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeAktif from './assets/home_activated.png';
 import HomeInaktif from './assets/home_inactive.png';
+import BagAktif from './assets/bag_activated.png';
+import baginaktif from './assets/bag_inactive.png';
+import favoritesAktif from './assets/favorites_activated.png';
+import favoritesinaktif from './assets/favorites_inactive.png';
+import profilAktif from './assets/profil_activated.png';
+import profilinaktif from './assets/profil_inactive.png';
+import shopAktif from './assets/shop_activated.png';
+import shopinaktif from './assets/shop_inactive.png';
 
 const Tab = createBottomTabNavigator();
 function MyTabs() {
@@ -24,12 +33,48 @@ function MyTabs() {
         }}
       />
       <Tab.Screen
-        name="Login"
-        component={LoginPage}
+        name="Bag"
+        component={HomeScreen}
         options={{
           headerShown: false, tabBarIcon: ({ focused }) => (
             <Image
-              source={focused ? HomeAktif : HomeInaktif}
+              source={focused ? BagAktif : baginaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="favorite"
+        component={HomeScreen}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? favoritesAktif : favoritesinaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="profil"
+        component={HomeScreen}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? profilAktif : profilinaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="shop"
+        component={HomeScreen}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? shopAktif : shopinaktif}
               style={{ width: 40, height: 40 }}
             />
           ),
@@ -57,8 +102,9 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="home" component={MyTabs} />
+        <Stack.Screen name="Home" component={MyTabs} />
         <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Bag" component={Bag} />
       </Stack.Navigator>
     </NavigationContainer>
   );
